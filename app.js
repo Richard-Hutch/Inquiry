7d165650ab3056c810580fe57cc7c0ee8d4ffa75
RichardHutcheson <richardhutch80@gmail.com>
2021-07-10 01:24:25 -0500

    user can log in

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-let client_id = "";
-let client_secret = "";
+let client_id = "45d081854dd645af9ace7d813d3f7ae4";
 let redirect_uri = "http://127.0.0.1:5500/mainpage.html";
+let authURL = "https://accounts.spotify.com/authorize" + "?client_id=" + client_id + "&response_type=token" + "&redirect_uri=" + encodeURI(redirect_uri) + "&show_dialog=true";
 
-const AUTHORIZE = "https://acccounts.spotify.com/authorize";
 
-function requestAuthorization(){
-    client_id = document.getElementById("clientId").value;
-    client_secret = document.getElementById("clientSecret").value;
-    //The localStorage object stores the data with no expiration date. 
-    //The data will not be deleted when the browser is closed, and will be available the next day, week, or year.
-    //localStorage.setItem("client_id", client_id);
-    //localStorage.setItem("client_secret", client_secret);
-    /*
-    The sessionStorage object is equal to the localStorage object, except that it stores the data for only one session. 
-    The data is deleted when the user closes the specific browser tab.
-    */
-    sessionStorage.setItem("client_id", client_id);
-    sessionStorage.setItem("client_secret", client_secret);
-    let url = AUTHORIZE;
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri" + encodeURI(redirect_uri);
-    url += "&show_dialog=true";
-    window.location.href = url; //show Spotify's authorization screen
+async function authorize(){
+    //console.log(authURL);
+    window.location = authURL;
 }

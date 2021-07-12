2ebac5d4d33eccd83b39396e43183fa8a8b623f9
Richard-Hutcheson <richardhutch80@gmail.com>
2021-07-11 21:15:53 -0500

    allowed for searching in search bar

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ let client_id = "45d081854dd645af9ace7d813d3f7ae4";
 let redirect_uri = "http://127.0.0.1:5500/mainpage.html";
 let authURL = "https://accounts.spotify.com/authorize" + "?client_id=" + client_id + "&response_type=token" + "&redirect_uri=" + encodeURI(redirect_uri) + "&show_dialog=true";
 
-
-async function authorize(){
+function authorize(){
     //console.log(authURL);
     window.location = authURL;
+}
+function search(){
+    let val = document.getElementById("search-input").value;
+    if (val === ""){
+        console.log("Search is empty");
+    }else{
+        console.log("searching for: " + val);
+    }
 }

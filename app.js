9a3a923e1bcadf5a2f27ae8270b229a15cdb691b
Richard-Hutcheson <richardhutch80@gmail.com>
2021-08-25 19:29:52 -0500

    test6

--- app.js
+++ app.js
@@ -601,7 +601,6 @@ async function fetchProfileDetails(plain = false){
         document.getElementById("profile-link-id").setAttribute("href", result.external_urls.spotify);
         document.getElementById("profile-link-id").setAttribute("target", "_blank"); //open to new tab
         if (result.images[0] != null){
-            console.log(result.images[0].url);
             document.getElementById("user-img-id").setAttribute("src", result.images[0].url);
             document.getElementById("profile-picture-div-id").classList.remove("profile-picture-blank");
             document.getElementById("profile-picture-div-id").classList.add("profile-picture-custom");
